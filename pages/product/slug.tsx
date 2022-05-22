b75494c11979396353028afec60a07ec302c8ca1
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2022-05-22 17:37:05 -0300

    pagina de size, count, empty

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -2,7 +2,8 @@ import { Button, Chip, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { ShopLayout } from "../../components/layauts";
-import { ProductSlideshow } from "../../components/products";
+import { ProductSlideshow, SizeSelector } from "../../components/products";
+import { ItemCounter } from "../../components/ui";
 import { initialData } from "../../database/products";
 
 const product = initialData.products[0];
@@ -37,6 +38,11 @@ const ProductPage = () => {
                                 Cantidad
                             </Typography>
                             {/* itemcounter */}
+                            <ItemCounter />
+                            <SizeSelector
+                            //selectedSize={product.sizes[0]}
+                            sizes={product.sizes}
+                            />
                         </Box>
                         {/* agregar al carrito */}
                         <Button color="secondary" className="circular-btn">
