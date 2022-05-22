6596b9614a7331aae01df90f299140cc1bfc9ec8
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2022-05-22 12:05:34 -0300

    se crea product list y productcard

--- components/layauts/ShopLayout.tsx
+++ components/layauts/ShopLayout.tsx
@@ -1,39 +1,45 @@
 import Head from "next/head";
 import { FC, ReactNode } from "react";
-import { Navbar } from "../ui";
+import { Navbar, SideMenu } from "../ui";
 
 interface Props {
-  title: string;
-  pageDescription: string;
-  imageFullUrl: string;
-  children?: ReactNode;
+    title: string;
+    pageDescription: string;
+    imageFullUrl: string;
+    children?: ReactNode;
 }
 
 export const ShopLayout: FC<Props> = ({
-  title,
-  pageDescription,
-  imageFullUrl,
-  children,
+    title,
+    pageDescription,
+    imageFullUrl,
+    children,
 }) => {
-  return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={pageDescription} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={pageDescription} />
-        {imageFullUrl && (
-          <meta property="og:image" content={imageFullUrl} />
-        )}
-      </Head>
-      <nav><Navbar/></nav>
-      {/* TODO: SIDEBAR */}
-      <main
-        style={{ margin: "80px auto", maxWidth: "1440px", padding: "0 30px" }}
-      >
-        {children}
-      </main>
-      <footer>{/* TODO: MI FOOTER */}</footer>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={pageDescription} />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={pageDescription} />
+                {imageFullUrl && (
+                    <meta property="og:image" content={imageFullUrl} />
+                )}
+            </Head>
+            <nav>
+                <Navbar />
+            </nav>
+            <SideMenu />
+            <main
+                style={{
+                    margin: "80px auto",
+                    maxWidth: "1440px",
+                    padding: "0 30px",
+                }}
+            >
+                {children}
+            </main>
+            <footer>{/* TODO: MI FOOTER */}</footer>
+        </>
+    );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,25 @@
 import { Typography } from "@mui/material";
 import type { NextPage } from "next";
 import { ShopLayout } from "../components/layauts";
+import { ProductList } from "../components/products";
+import { initialData } from "../database/products";
 
 const Home: NextPage = () => {
-  return (
-    <ShopLayout
-      title={"Shop-Nextjs - Home"}
-      pageDescription={"Encuentre los mejores productos de Shop aqui"}
-      imageFullUrl={"https://source.unsplash.com/random"}
-    >
-      <Typography variant="h1" component="h1">
-        Tienda
-      </Typography>
-      <Typography variant="h2" component="h2" sx={{ mb: 1 }}>
-        Todos los productos
-      </Typography>
-    </ShopLayout>
-  );
+    return (
+        <ShopLayout
+            title={"Shop-Nextjs - Home"}
+            pageDescription={"Encuentre los mejores productos de Shop aqui"}
+            imageFullUrl={"https://source.unsplash.com/random"}
+        >
+            <Typography variant="h1" component="h1">
+                Tienda
+            </Typography>
+            <Typography variant="h2" component="h2" sx={{ mb: 1 }}>
+                Todos los productos
+            </Typography>
+            <ProductList products={initialData.products as any} />
+        </ShopLayout>
+    );
 };
 
 export default Home;
