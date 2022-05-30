6b58a2ae358f112f9ab4cfd1ddec81e81e342d74
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2022-05-29 23:04:36 -0300

    conexion a api de busquedas

--- models/Product.ts
+++ models/Product.ts
@@ -40,6 +40,7 @@ const productSchema = new Schema(
 );
 
 //TODO: CREAR INDICE DE MONGO
+productSchema.index({ title: "text", tags: "text" });
 
 const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema);
 
